22ec04337382be8eaad0823e16cc0f3b5ee74129
jscarlett5 <31358293+jscarlett5@users.noreply.github.com>
2026-05-05 10:32:35 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': {},
-  },
   server: {
     host: '0.0.0.0',
     port: 3000,
-  },
+    hmr: {
+      clientPort: 5173 // Forces the browser to talk back on the correct port
+    }
+  }
 })
