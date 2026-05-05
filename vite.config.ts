54a5ff9b223534b9f54f6708bf15f62d5e0f2cb8
jscarlett5 <31358293+jscarlett5@users.noreply.github.com>
2026-05-05 10:35:05 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
     hmr: {
-      clientPort: 5173 // Forces the browser to talk back on the correct port
+      clientPort: 5173 // Forces the browser to communicate correctly
     }
   }
 })
